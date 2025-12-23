2cee738dcf777de943cbee7a35bc06867ae409f4
Marco <imarco.venegas@gmail.com>
2025-12-22 18:45:33 -0600

    Added output info for deploy s3

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,18 +1,27 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const exec = require('@actions/exec');
 
+async function run() {
+  try {
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
 
-function run() {
-  const bucket = core.getInput('bucket', { required: true });
-  const bucketRegion = core.getInput('bucket-region', { required: true });
-  const distFolder = core.getInput('dist-folder', { required: true });
+    const s3Uri = `s3://${bucket}`;
 
-  const s3Uri = `s3://${bucket}`
-  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+    await exec.exec(
+      'aws',
+      ['s3', 'sync', distFolder, s3Uri, '--region', bucketRegion],
+      { stdio: 'inherit' }
+    );
 
-  const websiteURl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
-  core.setOutput('website-url', websiteURl);
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+
+    core.setOutput('website-url', websiteUrl);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run();
