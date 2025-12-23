42496af29af5752dc053e38135afa22897c277dc
Marco <imarco.venegas@gmail.com>
2025-12-22 18:35:11 -0600

    Added output info for deploy s3

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -11,7 +11,8 @@ function run() {
   const s3Uri = `s3://${bucket}`
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-  core.notice('Javascript action log');
+  const websiteURl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+  core.setOutput('website-url', websiteURl);
 }
 
 run();
